a988575a26f8d2ecf896a51ca66490f413793072
phs6417 <phs6417@naver.com>
2016-09-18 23:59:47 +0900

    Update Searching.java

--- Searching.java
+++ Searching.java
@@ -2,6 +2,8 @@ public class Searching {
 	public static void main(String args[]) {
 		
 		// 검색어 입력 -> 검색어에 해당하는 단어 찾기 -> 1 0 boolean
+		
+		//substring , indexOf
 		String ex = "예시용단어example";
 		String search = 
 		for(
