8fd53ec14a8aa9b55f7868e2d3cd7ec04ba41106
phs6417 <phs6417@naver.com>
2016-09-19 23:34:55 +0900

    Update Searching.java

--- Searching.java
+++ Searching.java
@@ -9,16 +9,20 @@ public class Searching {
 		
 		//matches 
 		
-		String ex = "예시용단어example";
-		Scanner searching = new Scanner(System.in);
+		String ex = "예시용 단어 example word";
+//		Scanner searching = new Scanner(System.in);
 		
-		if(searching.matches(ex))
-			System.out.println(searching);
+//		if(ex.matches(.searching))
+//			System.out.println(searching);
+//		입력을 직접 받는형식 연구해보기
+//				
+		if(ex.matches(".*example.*"))
+			System.out.println("yes");
+		else
+			System.out.println("no");
 		
 				
 				
-				  
-				
-				)
 	}
 }
+
